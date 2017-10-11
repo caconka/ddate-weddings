b56c49564ca29cf41bc670e5800faeace094875d
Carlos Cabezas <carloscabezascontra@gmail.com>
2017-10-11 16:58:04 +0200

    shell script compiling

---
+++ compile.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+echo "Copy files to public..."
+cd client
+ng build --env=prod
+cd ..
+rm -rf ./server/public
+mkdir ./server/public
+mkdir ./server/public/uploads
+cp -r ./client/dist/ ./server/public/
+echo "Done!!"
