d18d348ff8a753793703587ef806b10cfe351ff1
Carlos Cabezas <carloscabezascontra@gmail.com>
2017-10-19 11:42:30 +0200

    compiling

--- server/api/user/diary/index.js
+++ server/api/user/diary/index.js
@@ -1,11 +1,10 @@
 const express = require('express');
 const router = express.Router();
-const { ensureLoggedIn, ensureLoggedOut } = require('connect-ensure-login');
-const checkRole = require('../../../middleware/check-role');
+const { ensureLoggedIn } = require('connect-ensure-login');
 const checkIdParams = require('../../../middleware/check-id');
 const Controller = require('./diary.controller')
 
-router.get('/:id/events', checkIdParams, Controller.getDiaryGet);
-router.post('/:id/add-event', checkIdParams, Controller.addEventPost);
+router.get('/:id/events', checkIdParams, ensureLoggedIn(), Controller.getDiaryGet);
+router.post('/:id/add-event', checkIdParams, ensureLoggedIn(), Controller.addEventPost);
 
 module.exports = router;
